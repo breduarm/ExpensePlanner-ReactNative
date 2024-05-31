f6a01caff7e1e1ecaa04189fac6dcfe6011aab06
breduarm <bryan.e.armijos@gmail.com>
2024-05-31 15:00:00 -0500

    Add ControlBudget component

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,30 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Alert, StyleSheet, Text, View} from 'react-native';
 import Header from './components/Header';
 import NewBudget from './components/NewBudget';
+import ControlBudget from './components/ControlBudget';
 
 function App(): React.JSX.Element {
+  const [isBudgetValid, setIsBudgetValid] = useState(false);
 
-  const handleNewBudget = (budget: number)=> {
-    console.log("=== new Budget: ", budget)
+  const handleNewBudget = (budget: number) => {
+    console.log('=== new Budget: ', budget);
     if (budget > 0) {
-      console.log("Valid Budget")
+      setIsBudgetValid(true);
     } else {
-      Alert.alert("Error", "The budget must be greater than 0")
+      Alert.alert('Error', 'The budget must be greater than 0');
     }
-  }
+  };
 
   return (
     <View style={styles.container}>
       <View style={styles.header}>
         <Header />
-        <NewBudget handleNewBudget={handleNewBudget} />
+        {isBudgetValid ? (
+          <ControlBudget />
+        ) : (
+          <NewBudget handleNewBudget={handleNewBudget} />
+        )}
       </View>
     </View>
   );
@@ -31,7 +37,7 @@ const styles = StyleSheet.create({
   },
   header: {
     backgroundColor: '#3B82F6',
-},
+  },
 });
 
 export default App;
---
+++ src/components/ControlBudget.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import {Text} from 'react-native';
+
+const ControlBudget = () => {
+  return <Text> From ControlBudget </Text>;
+};
+
+export default ControlBudget;
