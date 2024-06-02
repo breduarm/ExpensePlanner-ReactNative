e05d41449bb50a45af781eea207a4ae043d699fb
breduarm <bryan.e.armijos@gmail.com>
2024-06-01 23:35:20 -0500

    Prepare App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  Text,
+} from 'react-native';
+
+function App(): React.JSX.Element {
+
+  return (
+    <SafeAreaView>
+      <Text>Expense Planner App</Text>
+    </SafeAreaView>
+  );
+}
+
+export default App;
