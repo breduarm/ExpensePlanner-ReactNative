7e54d227af2ca8a17e470f4c925a5b3ecb5ccf61
breduarm <bryan.e.armijos@gmail.com>
2024-06-04 17:40:55 -0500

    Add styles to ExpenseForm component

--- src/components/ExpenseForm.tsx
+++ src/components/ExpenseForm.tsx
@@ -1,47 +1,101 @@
-import React from 'react'
-import { Pressable, SafeAreaView, StyleSheet, Text, TextInput, View } from 'react-native'
+import React from 'react';
+import {
+  Pressable,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from 'react-native';
 
-import { Picker } from '@react-native-picker/picker'
+import {Picker} from '@react-native-picker/picker';
+import globalStyles from '../styles';
 
 const ExpenseForm = () => {
   return (
-    <SafeAreaView>
-        <View>
-            <Pressable>
-                <Text></Text>
-            </Pressable>
+    <SafeAreaView style={styles.container}>
+      <View>
+        <Pressable>
+          <Text>Cancel</Text>
+        </Pressable>
+      </View>
+      <View style={styles.form}>
+        <Text style={styles.title}>New Expense</Text>
+        <View style={styles.field}>
+          <Text style={styles.label}>Expense name:</Text>
+          <TextInput
+            style={styles.input}
+            placeholder="Name of the expense e.g. Food"
+          />
         </View>
-        <View>
-            <Text>New Expense</Text>
-            <View>
-                <Text>Expense name:</Text>
-                <TextInput placeholder='Name of the expense e.g. Food' />
-            </View>
-            <View>
-                <Text>Expense amount:</Text>
-                <TextInput placeholder='Amount of the expense e.g. 300' />
-            </View>
-            <View>
-                <Text>Expense category:</Text>
-                <Picker>
-                    <Picker.Item label='-- Select --' value='' />
-                    <Picker.Item label='Savings' value='savings' />
-                    <Picker.Item label='Food' value='food' />
-                    <Picker.Item label='Health' value='health' />
-                    <Picker.Item label='Subscriptions' value='subscriptions' />
-                    <Picker.Item label='Other' value='other' />
-                </Picker>
-            </View>
-            <Pressable>
-                <Text>Add Expense</Text>
-            </Pressable>
+        <View style={styles.field}>
+          <Text style={styles.label}>Expense amount:</Text>
+          <TextInput
+            style={styles.input}
+            placeholder="Amount of the expense e.g. 300"
+          />
         </View>
+        <View style={styles.field}>
+          <Text style={styles.label}>Expense category:</Text>
+          <Picker>
+            <Picker.Item label="-- Select --" value="" />
+            <Picker.Item label="Savings" value="savings" />
+            <Picker.Item label="Food" value="food" />
+            <Picker.Item label="Health" value="health" />
+            <Picker.Item label="Subscriptions" value="subscriptions" />
+            <Picker.Item label="Other" value="other" />
+          </Picker>
+        </View>
+        <Pressable style={styles.submitBtn}>
+          <Text style={styles.submitBtnText}>Add Expense</Text>
+        </Pressable>
+      </View>
     </SafeAreaView>
-  )
-}
+  );
+};
 
 const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#1E40AF',
+    flex: 1,
+  },
+  form: {
+    ...globalStyles.container,
+  },
+  title: {
+    textAlign: 'center',
+    fontSize: 28,
+    marginBottom: 32,
+    color: '#64748B',
+  },
+  field: {
+    marginVertical: 8,
+  },
+  label: {
+    color: '#64748B',
+    textTransform: 'uppercase',
+    fontSize: 16,
+    fontWeight: 'bold'
+  },
+  input: {
+    height: 40,
+    backgroundColor: '#F5F5F5',
+    borderRadius: 8,
+    marginTop: 8,
+  },
+  submitBtn: {
+    height: 40,
+    marginTop: 20,
+    justifyContent: 'center',
+    backgroundColor: '#3B82F6',
+    borderRadius: 8,
+  },
+  submitBtnText: {
+    textAlign: 'center',
+    color: '#FFFFFF',
+    fontWeight: '700',
+    textTransform: 'uppercase',
+  },
+});
 
-})
-
-export default ExpenseForm
+export default ExpenseForm;
