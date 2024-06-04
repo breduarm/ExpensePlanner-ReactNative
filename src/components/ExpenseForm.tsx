2cee47deadc3e13e49cba1de0a26e27a53aa9f9e
breduarm <bryan.e.armijos@gmail.com>
2024-06-04 17:22:48 -0500

    Implement picker and add structure to ExpenseForm

--- src/components/ExpenseForm.tsx
+++ src/components/ExpenseForm.tsx
@@ -1,12 +1,47 @@
 import React from 'react'
-import { SafeAreaView, Text } from 'react-native'
+import { Pressable, SafeAreaView, StyleSheet, Text, TextInput, View } from 'react-native'
+
+import { Picker } from '@react-native-picker/picker'
 
 const ExpenseForm = () => {
   return (
     <SafeAreaView>
-        <Text>From Expense form</Text>
+        <View>
+            <Pressable>
+                <Text></Text>
+            </Pressable>
+        </View>
+        <View>
+            <Text>New Expense</Text>
+            <View>
+                <Text>Expense name:</Text>
+                <TextInput placeholder='Name of the expense e.g. Food' />
+            </View>
+            <View>
+                <Text>Expense amount:</Text>
+                <TextInput placeholder='Amount of the expense e.g. 300' />
+            </View>
+            <View>
+                <Text>Expense category:</Text>
+                <Picker>
+                    <Picker.Item label='-- Select --' value='' />
+                    <Picker.Item label='Savings' value='savings' />
+                    <Picker.Item label='Food' value='food' />
+                    <Picker.Item label='Health' value='health' />
+                    <Picker.Item label='Subscriptions' value='subscriptions' />
+                    <Picker.Item label='Other' value='other' />
+                </Picker>
+            </View>
+            <Pressable>
+                <Text>Add Expense</Text>
+            </Pressable>
+        </View>
     </SafeAreaView>
   )
 }
+
+const styles = StyleSheet.create({
+
+})
 
 export default ExpenseForm
